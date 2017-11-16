925866197619c42ae29f6fb601b268e4953d59c6
josepcasadomrf <josep.casado@marfeel.com>
2017-11-17 00:48:55 +0100

    Board.java

--- src/Board.java
+++ src/Board.java
@@ -1,16 +1,177 @@
-
+import java.lang.reflect.Array;
+import java.util.Random;
+import java.util.ArrayList;
 
 public class Board {
-    public Board(int[][] blocks)           // construct a board from an n-by-n array of blocks
-    // (where blocks[i][j] = block in row i, column j)
-    public int dimension()                 // board dimension n
-    public int hamming()                   // number of blocks out of place
-    public int manhattan()                 // sum of Manhattan distances between blocks and goal
-    public boolean isGoal()                // is this board the goal board?
-    public Board twin()                    // a board that is obtained by exchanging any pair of blocks
-    public boolean equals(Object y)        // does this board equal y?
-    public Iterable<Board> neighbors()     // all neighboring boards
-    public String toString()               // string representation of this board (in the output format specified below)
 
-    public static void main(String[] args) // unit tests (not graded)
+    int[][] blocks;
+    int[][] manhattanDistances;
+    int blankPosition;
+
+    public Board(int[][] blocks) {
+        this.blocks = blocks;
+        this.manhattanDistances = new int[blocks.length][blocks.length];
+        this.initManhattanDistances();
+        this.findBlankPosition();
+    }
+
+    public int dimension() {
+        return this.blocks.length;
+    }
+
+    public int hamming() {
+        int blocksOutOfPlace = 0;
+        for (int i = 0; i < this.dimension(); i++)
+            for (int j = 0; j < this.dimension(); j++)
+                if (isBlockOutOfPlace(this.blocks[i][j], i, j)) blocksOutOfPlace++;
+
+        return blocksOutOfPlace;
+    }
+
+    public int manhattan() {
+        int sum = 0;
+        for (int i = 0; i < this.dimension(); i++)
+            for (int j = 0; j < this.dimension(); j++)
+                sum += this.manhattanDistances[i][j];
+
+        return sum;
+    }
+
+    public boolean isGoal() {
+        for (int i = 0; i < this.dimension(); i++)
+            for (int j = 0; j < this.dimension(); j++)
+                if (isBlockOutOfPlace(this.blocks[i][j], i, j)) return false;
+
+        return true;
+    }
+
+    public Board twin() {
+
+        Random rand = new Random();
+        int n = this.dimension() * this.dimension();
+        int randomPosition = rand.nextInt(n);
+        if (randomPosition == this.blankPosition) randomPosition = (randomPosition + 1) % n;
+        int randomPosition2 = rand.nextInt(n);
+        if (randomPosition2 == this.blankPosition) randomPosition2 = (randomPosition2 + 1) % n;
+        if (randomPosition2 == randomPosition) randomPosition2 = (randomPosition2 + 1) % n;
+
+        int[][] twinBlocks = this.copyBlocks();
+
+        int number1 = twinBlocks[getRowFromPosition(randomPosition)][getColFromPosition(randomPosition)];
+        int number2 = twinBlocks[getRowFromPosition(randomPosition2)][getColFromPosition(randomPosition2)];
+        twinBlocks[getRowFromPosition(randomPosition)][getColFromPosition(randomPosition)] = number2;
+        twinBlocks[getRowFromPosition(randomPosition2)][getColFromPosition(randomPosition2)] = number1;
+
+        return new Board(twinBlocks);
+
+    }
+
+    public boolean equals(Object y) {
+
+        Board board = (Board) y;
+        if (this.dimension() != board.dimension()) return false;
+        for (int i = 0; i < this.dimension(); i++)
+            for (int j = 0; j < this.dimension(); j++)
+                if (this.blocks[i][j] != board.blocks[i][j]) return false;
+
+        return true;
+    }
+
+
+    public Iterable<Board> neighbors() {
+
+        ArrayList<Board> neighboringBoards = new ArrayList<>();
+        for (int position : neighborsOfBlankPosition()) {
+            neighboringBoards.add(newBoardFromMove(position));
+        }
+
+        return neighboringBoards;
+    }
+
+    public String toString() {
+
+        String res = "" + this.dimension() + "\n";
+
+        for (int i = 0; i < this.dimension(); i++) {
+            for (int j = 0; j < this.dimension(); j++) {
+                res += " " + this.blocks[i][j] + " ";
+            }
+            res += "\n";
+        }
+
+        return res;
+    }
+
+    private ArrayList<Integer> neighborsOfBlankPosition() {
+
+        ArrayList<Integer> neighbors = new ArrayList<>();
+        int row = this.blankPosition / this.dimension();
+        int col = this.blankPosition % this.dimension();
+        if (row + 1 < this.dimension()) neighbors.add(getPositionFromRowAndCol(row + 1, col));
+        if (row - 1 >= 0) neighbors.add(getPositionFromRowAndCol(row - 1, col));
+        if (col + 1 < this.dimension()) neighbors.add(getPositionFromRowAndCol(row, col + 1));
+        if (col - 1 >= 0) neighbors.add(getPositionFromRowAndCol(row, col - 1));
+
+        return neighbors;
+    }
+
+    private int manhattanDistance(int number, int currentRow, int currentCol) {
+        int n = this.dimension();
+        number--;
+        int goalRow = number / n;
+        int goalCol = number % n;
+        return Math.abs(currentCol - goalCol) + Math.abs(currentRow - goalRow);
+    }
+
+    private void initManhattanDistances() {
+        for (int i = 0; i < this.dimension(); i++)
+            for (int j = 0; j < this.dimension(); j++)
+                this.manhattanDistances[i][j] = manhattanDistance(this.blocks[i][j], i, j);
+    }
+
+    private void findBlankPosition() {
+        for (int i = 0; i < this.dimension(); i++)
+            for (int j = 0; j < this.dimension(); j++)
+                if (this.blocks[i][j] == 0) this.blankPosition = i + j;
+    }
+
+    private boolean isBlockOutOfPlace(int number, int row, int column) {
+        return getPositionFromRowAndCol(row, column) == --number;
+    }
+
+    private int getPositionFromRowAndCol(int row, int col) {
+        return row * this.dimension() + col;
+    }
+
+    private int getRowFromPosition(int position) {
+        return position / this.dimension();
+    }
+
+    private int getColFromPosition(int position) {
+        return position % this.dimension();
+    }
+
+    private int[][] copyBlocks() {
+        int[][] newBlocks = new int[this.dimension()][this.dimension()];
+        for (int i = 0; i < this.dimension(); i++) {
+            System.arraycopy(this.blocks[i], 0, newBlocks[i], 0, this.dimension());
+        }
+        return newBlocks;
+    }
+
+    private Board newBoardFromMove(int position) {
+        int[][] newBlocks = copyBlocks();
+        int row = getRowFromPosition(position);
+        int col = getColFromPosition(position);
+        int value = newBlocks[row][col];
+        newBlocks[row][col] = 0;
+        newBlocks[getRowFromPosition(this.blankPosition)][getColFromPosition(this.blankPosition)] = value;
+
+        return new Board(newBlocks);
+    }
+
+    public static void main(String[] args) {
+
+    }
+
 }
